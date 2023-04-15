ad7444f441917b5f03e4a56fbeb0504e9821bcf4
Ángela García <97111035+garciahuelamo@users.noreply.github.com>
2023-04-15 19:37:00 +0200

    Add files via upload
    230415 - made the JavaDoc

---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Controlador"},{"p":"<Unnamed>","l":"ListaVideojuegos"},{"p":"<Unnamed>","l":"MetodosCRUDObjectList"},{"p":"<Unnamed>","l":"Utilidades"},{"p":"<Unnamed>","l":"Videojuegos"}];updateSearchResults();
